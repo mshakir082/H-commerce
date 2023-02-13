d5a70734cbe9bcc2b7cd3be0c6606c6346c7666c
YCTechDev <yctech@yellowchalk.com>
2023-02-13 16:58:26 +0530

    card page

--- src/component/Layout/NavList.jsx
+++ src/component/Layout/NavList.jsx
@@ -1,12 +1,15 @@
 import classes from "./NavList.module.css";
 import { Link } from "react-router-dom";
 const NavList = () => {
+  const handelClick = ()=>{
+    console.log('shakir');
+  }
   return (
     <div className={classes.Navlist}>
       <h1 className={classes.NavList_text}>
         <Link to="/products">Products</Link>
       </h1>
-      <h1 className={classes.NavList_text}>
+      <h1 className={classes.NavList_text} onClick={handelClick}>
         <Link to="/cart">Cart</Link>
       </h1>
     </div>
