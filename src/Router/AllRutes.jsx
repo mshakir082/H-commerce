5b0c16a48940f68749a3485956779909f7a09e6c
YCTechDev <yctech@yellowchalk.com>
2023-03-01 17:12:23 +0530

    home page Done

--- src/Router/AllRutes.jsx
+++ src/Router/AllRutes.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Products from "../pages/Products";
+import Product from "../component/Products/product";
 import Cart from "../pages/Cart";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
@@ -8,7 +8,7 @@ const AllRutes = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/products" element={<Products />} />
+      <Route path="/products" element={<Product />} />
       <Route path="/cart" element={<Cart />} />
     </Routes>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,64 @@
-import Generics from '../component/Layout/Generics'
-import Products from '../pages/Products';
-import SeeCart from '../component/Cart/SeeCart'
-import Footer from './Footer';
+import Generics from "../component/Layout/Generics";
+import Footer from "./Footer";
 const Home = () => {
-  const HStyle ={
-    textAlign:'center',
-    color:'white',
-    fontSize:'44px'
+  const HStyle = {
+    textAlign: "center",
+    color: "white",
+    fontSize: "44px",
   };
-  
+  const data = [
+    {
+      date: "JUL16",
+      title: "DETROIT, MI",
+      describe: "DTE ENERGY MUSIC THEATRE",
+    },
+    {
+      date: "JUL19",
+      title: "BRISTOW, VA",
+      describe: "BUDWEISER STAGE",
+    },
+    {
+      date: "JUL 22",
+      title: "PHOENIX, AZ",
+      describe: "JIGGY LUBE LIVE",
+    },
+    {
+      date: "JUL 29",
+      title: "LAS VEGAS, NV",
+      describe: "AK-CHIN PAVILION",
+    },
+    {
+      date: "AUG 2",
+      title: "DETROIT, MI",
+      describe: "T-MOBILE ARENA",
+    },
+    {
+      date: "AUG 7",
+      title: "CONCORD, CA",
+      describe: "CONCORD PAVILION",
+    },
+  ];
   return (
     <>
       {/* <h1>Home</h1> */}
       <Generics>
         <h1 style={HStyle}>The Generics</h1>
       </Generics>
-      <Products></Products>
-      <div style={{textAlign:'center',margin:'-58px 0px 35px'}}>
-        <SeeCart/>
-      </div>
+      <h1 style={{ textAlign: "center" }}>TOURS</h1>
+      {data.map((el)=>(
+        <>
+        <div style={{display:'flex',justifyContent:'space-around',margin:'40px 0px'}}>
+        <h5>{el.date}</h5>
+        <div>{el.title}</div>
+        <div>{el.describe}</div>
+        <div><button>BUY TICKETS</button></div>
+       </div>
+      <hr></hr>
+      </>
+      ))}
+      
       <div>
-        <Footer/>
+        <Footer />
       </div>
     </>
   );
